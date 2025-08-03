b81e6517e4ce85bbd6dd3a44b69f5c20c118d8ed
MathDivergent <math.divergent@gmail.com>
2025-08-03 22:16:39 +0300

    Up

--- Development/Engine/Core/Main.cpp
+++ Development/Engine/Core/Main.cpp
@@ -74,7 +74,7 @@ int fMain(int iArgumentCount, char** pArgumentValues)
     #if EIGHTGINE_PLATFORM_WINDOWS
     for (auto const& plugin : plugins)
     {
-        std::string fullName = plugin + ".dll";
+        std::string fullName = std::string(EIGHTGINE_PROJECT_EXECUTABLE_DIR) + plugin + ".dll";
         HMODULE hGameModule = LoadLibrary(fullName.c_str());
         if (!hGameModule)
         {
@@ -91,7 +91,7 @@ int fMain(int iArgumentCount, char** pArgumentValues)
     #if EIGHTGINE_PLATFORM_LINUX
     for (const auto& plugin : plugins)
     {
-        std::string fullName = plugin + ".so";
+        std::string fullName = std::string(EIGHTGINE_PROJECT_EXECUTABLE_DIR) + plugin + ".so";
         void* handle = dlopen(fullName.c_str(), RTLD_NOW);
         if (!handle)
         {
@@ -115,7 +115,7 @@ int fMain(int iArgumentCount, char** pArgumentValues)
     #if EIGHTGINE_PLATFORM_MACOS
     for (const auto& plugin : plugins)
     {
-        std::string fullName = plugin + ".dylib";
+        std::string fullName = std::string(EIGHTGINE_PROJECT_EXECUTABLE_DIR) + plugin + ".dylib";
         void* handle = dlopen(fullName.c_str(), RTLD_NOW);
         if (!handle)
         {

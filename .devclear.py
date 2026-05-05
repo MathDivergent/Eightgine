a11852be069a1c6d36262950db27ed401171e9cc
MathDivergent <math.divergent@gmail.com>
2026-05-05 22:09:37 +0300

    Upload Eightgine v0.3.0.

--- .devclear.py
+++ .devclear.py
@@ -1,42 +1,85 @@
 #!/usr/bin/env python3
+import io
+import fnmatch
 import os
-import shutil
 import platform
+import shutil
 
-FILES = ["CMakeLists.txt.user"]
-FOLDERS = ["Intermediate", ".idea"]
 
-def remove_folders(folders):
-    for folder in folders:
-        if os.path.exists(folder) and os.path.isdir(folder):
-            try:
-                shutil.rmtree(folder)
-                print(f"Folder '{folder}' deleted.")
-            except Exception as e:
-                print(f"Error deleting folder '{folder}': {e}")
-        else:
-            print(f"Folder '{folder}' not found.")
+EIGHTGINE_PLATFORM_WINDOWS : bool = platform.system() == "Windows"
+EIGHTGINE_PLATFORM_LINUX : str = platform.system() == "Linux"
+EIGHTGINE_PLATFORM_MACOS : str = platform.system() == "Darwin"
 
-def remove_files(files):
-    for file in files:
-        if os.path.exists(file) and os.path.isfile(file):
-            try:
-                os.remove(file)
-                print(f"File '{file}' deleted.")
-            except Exception as e:
-                print(f"Error deleting file '{file}': {e}")
-        else:
-            print(f"File '{file}' not found.")
+class DDevClear:
+    def __init__(self):
+        self.__cDirPatterns: list[str] = [
+            "Intermediate",
+            ".idea"
+        ]
+
+        self.__cFilePatterns: list[str] = [
+            "CMakeLists.txt.user"
+        ]
+
+        self.__aRemoveStat: io.StringIO = io.StringIO()
+
+    @staticmethod
+    def __FileNameMatch(sFileName: str, cFilePatterns: list[str]):
+        return any(fnmatch.fnmatch(sFileName, sFilePattern) for sFilePattern in cFilePatterns)
+
+    def RemoveDirs(self):
+        for aParentDir, cChildDirs, _ in os.walk('.', topdown=True):
+            cMatchedDirs = [
+                aChildDir for aChildDir in cChildDirs 
+                if DDevClear.__FileNameMatch(aChildDir, self.__cDirPatterns)
+            ]
+
+            for aMatchedDir in cMatchedDirs:
+                aMatchedDirPath = os.path.join(aParentDir, aMatchedDir)
+                try:
+                    shutil.rmtree(aMatchedDirPath)
+                    cChildDirs.remove(aMatchedDir)
+                    print(f"Directory '{aMatchedDirPath}' deleted.", file=self.__aRemoveStat)
+                except Exception as aError:
+                    print(f"Error deleting directory '{aMatchedDirPath}': '{aError}'!", file=self.__aRemoveStat)
+
+    def RemoveFiles(self):
+        for aParentDir, _, cChildFiles in os.walk('.'):
+            cMatchedFiles = [
+                aChildFile for aChildFile in cChildFiles 
+                if DDevClear.__FileNameMatch(aChildFile, self.__cFilePatterns)
+            ]
+
+            for aMatchedFile in cMatchedFiles:
+                aMatchedFilePath = os.path.join(aParentDir, aMatchedFile)
+                try:
+                    os.remove(aMatchedFilePath)
+                    print(f"File '{aMatchedFilePath}' deleted.", file=self.__aRemoveStat)
+                except Exception as aError:
+                    print(f"Error deleting file '{aMatchedFilePath}': '{aError}'!", file=self.__aRemoveStat)
+
+    def RemoveStat(self):
+        sRemoveStat: str = self.__aRemoveStat.getvalue()
+        self.__aRemoveStat.close()
+
+        print(sRemoveStat, "Remove succeeded!")
+
 
 def main():
-    remove_folders(FOLDERS)
-    remove_files(FILES)
-    print("Remove succeeded!")
+    aDevClear: DDevClear = DDevClear()
+    aDevClear.RemoveDirs()
+    aDevClear.RemoveFiles()
+    aDevClear.RemoveStat()
 
-if __name__ == "__main__":
-    main()
-    if platform.system() == "Windows":
+    if EIGHTGINE_PLATFORM_WINDOWS:
         try:
             input("Press Enter to exit...")
         except EOFError:
             pass
+
+
+if __name__ == "__main__":
+    if EIGHTGINE_PLATFORM_WINDOWS or EIGHTGINE_PLATFORM_LINUX or EIGHTGINE_PLATFORM_MACOS:
+        main()
+    else:
+        print(f"Unsupported system: '{platform.system()}'!", file=self.aInfoStat)
